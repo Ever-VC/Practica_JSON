217c98372386c022fbee39e29315593a2f387289
Ever-VC <ever-vasquez27@hotmail.com>
2023-05-23 13:30:43 -0600

    Plantilla inicial

---
+++ escuadron.js
@@ -0,0 +1,14 @@
+const datosEscuadron = (datos) => {
+    const h1 = document.createElement("h1");
+    const h2 = document.createElement("h2");
+
+    h1.textContent = datos["squadName"];
+    h2.textContent = datos["secretBase"];
+
+    const header = document.querySelector("[data-escuadron]");
+
+    header.appendChild(h1);
+    header.appendChild(h2);
+}
+
+export default datosEscuadron;
